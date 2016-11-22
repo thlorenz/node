b6142e5c13cf9db0bed88c0e93def7a4517aa1d1
Thorsten Lorenz <thlorenz@gmx.de>
2016-11-22 15:17:12 -0300

    async-hooks: crypto.pbkdf2 tests

---
+++ test/async-hooks/test-crypto-pbkdf2.js
@@ -0,0 +1,38 @@
+const common = require('../common')
+const assert = require('assert')
+const initHooks = require('./init-hooks')
+const crypto = require('crypto')
+
+if (!common.hasCrypto) {
+  common.skip('missing crypto')
+  return
+}
+
+const hooks = initHooks()
+
+hooks.enable()
+crypto.pbkdf2('password', 'salt', 1, 20, 'sha256', onpbkdf2)
+
+
+function onpbkdf2() {
+  const as = hooks.activities()
+  const a = as[0]
+  assert.equal(a.after, null, 'never called after while in callback')
+  assert.equal(a.destroy, null, 'never called destroy while in callback')
+}
+
+process.on('exit', onexit)
+
+function onexit() {
+  hooks.disable()
+  hooks.sanityCheck()
+
+  const as = hooks.activities()
+  assert.equal(as.length, 1, 'one activity')
+
+  const a = as[0]
+  assert.equal(a.type, 'PBKDF2REQUEST', 'random byte request')
+  assert.equal(typeof a.uid, 'number', 'uid is a number')
+  assert.equal(a.parentUid, 1, 'parent uid 1')
+  assert.equal(a.before.length, 1, 'called before once')
+}
