02a7330c4f07c74a722546657d724eeff0f0e0b8
Thorsten Lorenz <thlorenz@gmx.de>
2016-11-23 11:04:08 -0300

    async-hooks: fs event wrap test

---
+++ test/async-hooks/test-fseventwrap.js
@@ -0,0 +1,36 @@
+const common = require('../common')
+const assert = require('assert')
+const initHooks = require('./init-hooks')
+const fs = require('fs')
+
+if (!common.hasCrypto) {
+  common.skip('missing crypto')
+  return
+}
+
+const hooks = initHooks()
+
+hooks.enable()
+const watcher = fs.watch(__dirname, onwatcherChanged);
+function onwatcherChanged() { }
+
+watcher.close()
+
+process.on('exit', onexit)
+
+function onexit() {
+  hooks.disable()
+  hooks.sanityCheck()
+
+  const as = hooks.activities()
+  assert.equal(as.length, 1, 'one activity')
+
+  const a = as[0]
+  assert.equal(a.type, 'FSEVENTWRAP', 'fs event wrap')
+  assert.equal(typeof a.uid, 'number', 'uid is a number')
+  assert.equal(a.parentUid, 1, 'parent uid 1')
+  assert.equal(a.init.length, 1, 'called init once')
+  assert.equal(a.before, null, 'never called before')
+  assert.equal(a.after, null, 'never called after')
+  assert.equal(a.destroy.length, 1, 'called destroy once')
+}
